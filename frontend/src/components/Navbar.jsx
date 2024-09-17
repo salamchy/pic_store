89b00a9469e8f0183d43414981e55b3f1050b755
salam <chaudharysm00@gmail.com>
2024-09-17 21:19:10 +0545

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="flex flex-col sm:flex-row justify-between items-start sm:items-center px-5 py-5 fixed top-0 left-0 right-0 shadow-md gap-1 sm:gap-0">
+      {/* logo name */}
+      <div className="font-bold text-3xl">
+        <Link to="/">CDYSEC</Link >
+      </div>
+
+      {/* list of nav */}
+      <ul className="flex gap-5 text-lg font-semibold text-gray-600 ml-5">
+        <li>About</li>
+        <li>Contact</li>
+        <li>Log In</li>
+        <li>Sign Up</li>
+      </ul>
+    </nav>
   )
 }
 export default Navbar;
